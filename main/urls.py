c7392bb1d9ecef55490759689b7f551276524ed4
le anh <leanh232002@gmail.com>
2023-04-16 11:38:29 +0700

    read / update sinh vien

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index),
-    path('profile/', views.profile)
-}
+    path('profile/', views.profile),
+    path('sinhviens/', views.sinhviens),
+    path('sinhviens/<int:id>/', views.sinhvien_detail),
+    path('sinhviens/<int:id>/update/', views.update_sinhvien),
+]
--- main/views.py
+++ main/views.py
@@ -1,8 +1,45 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render, get_object_or_404
+from main.models import SinhVien
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'index.html');
+    return render(request, 'index.html')
+
 
 def profile(request):
     return render(request, 'profile.html')
+
+
+def sinhviens(request):
+    sinhviens = SinhVien.objects.all()
+    context = {"sinhviens": sinhviens}
+    return render(request, 'sinhvien.html', context)
+
+
+def sinhvien_detail(request, id):
+    sinhvien = SinhVien.objects.filter(id=id)[0]
+    sinhvien.NgaySinh = sinhvien.NgaySinh.strftime("%Y-%m-%d")
+    context = {"sinhvien": sinhvien}
+    return render(request, 'sinhvien_detail.html', context)
+
+
+def update_sinhvien(request, id):
+    sinhvien = get_object_or_404(SinhVien, pk=id)
+
+    if request.method == 'POST':
+        # Update the SinhVien object with the data submitted in the form
+        sinhvien.HoTen = request.POST['hoten']
+        sinhvien.GioiTinh = request.POST['gioitinh']
+        sinhvien.NgaySinh = request.POST['ngaysinh']
+        sinhvien.DiaChi = request.POST['diachi']
+        sinhvien.Email = request.POST['email']
+        sinhvien.SoDienThoai = request.POST['sodienthoai']
+        sinhvien.MaPhong_id = request.POST['maphong']
+        sinhvien.save()
+
+        # Redirect to the detail page for the updated SinhVien object
+        return redirect(sinhvien_detail, id=id)
+
+    # Render the update SinhVien form with the current data for the SinhVien object
+    return render(request, 'sinhvien_detail.html', {'sinhvien': sinhvien})
